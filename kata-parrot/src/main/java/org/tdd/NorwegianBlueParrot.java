a1d51dd30bcd28f1962b13d70afe436f6f6f1749
Pascual Cano <pascual@strata3.com>
2016-07-15 19:33:31 +0200

    getSpeed overriden in Norweigian Blue Parrot subclass

--- kata-parrot/src/main/java/org/tdd/NorwegianBlueParrot.java
+++ kata-parrot/src/main/java/org/tdd/NorwegianBlueParrot.java
@@ -7,4 +7,9 @@ public class NorwegianBlueParrot extends Parrot {
     public NorwegianBlueParrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
+    }
 }
