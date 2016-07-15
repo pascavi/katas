3f9fd8c637de9ee6523123f12649c04d98b8ddf5
Pascual Cano <pascual@strata3.com>
2016-07-15 19:27:04 +0200

    Euroean Parrot subclass

--- kata-parrot/src/test/java/org/tdd/ParrotTest.java
+++ kata-parrot/src/test/java/org/tdd/ParrotTest.java
@@ -8,19 +8,19 @@ public class ParrotTest {
 
     @Test
     public void getSpeedOfEuropeanParrot() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+        Parrot parrot = new EuropeanParrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
         Assert.assertEquals(parrot.getSpeed(), 12.0,0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+        Parrot parrot = new AfricanParrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
         Assert.assertEquals(parrot.getSpeed(), 3.0,0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+        Parrot parrot = new NorwegianBlueParrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
         Assert.assertEquals(parrot.getSpeed(), 0.0,0);
     }
 
