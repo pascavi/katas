50c270b65a72700b8f51895ba91bab8cd3702851
Pascual Cano <pascual@strata3.com>
2016-07-15 20:13:43 +0200

    Moved getBaseSpeed method to NorwegianBlueParrot class

--- kata-parrot/src/main/java/org/tdd/NorwegianBlueParrot.java
+++ kata-parrot/src/main/java/org/tdd/NorwegianBlueParrot.java
@@ -12,4 +12,8 @@ public class NorwegianBlueParrot extends Parrot {
     public double getSpeed() {
         return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
+
+    protected double getBaseSpeed(double voltage) {
+        return Math.min(24.0, voltage *getBaseSpeed());
+    }
 }
