f8cca74f70d6596f6a56d1eb9119cddc6927bec0
Pascual Cano <pascual@strata3.com>
2016-11-25 19:17:39 +0100

    initial

--- kata-print-date/src/test/java/org/tdd/PrintDateTest.java
+++ kata-print-date/src/test/java/org/tdd/PrintDateTest.java
@@ -2,7 +2,6 @@ package org.tdd;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -11,8 +10,6 @@ public class PrintDateTest {
     PrintDate printDate;
     @Before
     public void setUp() {
-        //  create mock
-        printDate = Mockito.mock(PrintDate.class);
     }
 
 
