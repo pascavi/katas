5ec3c4f00fd25ea1da083b82af751dad7e438d3c
Pascual Cano <pascual@strata3.com>
2016-11-26 01:30:48 +0100

    Putting some order after the kata

---
+++ kata-print-date/src/test/java/org/tdd/DateMock.java
@@ -0,0 +1,14 @@
+package org.tdd;
+
+import java.util.Date;
+
+/**
+ * Created by Pascual on 26/11/2016.
+ */
+class DateMock extends Date {
+
+    @Override
+    public String toString() {
+        return "Fri Nov 25 19:34:55 CET 2016";
+    }
+}
--- kata-print-date/src/test/java/org/tdd/PrintDateTest.java
+++ kata-print-date/src/test/java/org/tdd/PrintDateTest.java
@@ -1,69 +1,31 @@
 package org.tdd;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.*;
 import java.util.Date;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PrintDateTest {
-    private class PrintStreamMock extends PrintStream {
-
-        private String lastStringPrinted;
-
-        PrintStreamMock(File dummy) throws FileNotFoundException {
-            super(new OutputStream() {
-                @Override
-                public void write(int b) throws IOException {
-
-                }
-            });
-        }
-
-        public void println(String input) {
-            // Manual stub
-            lastStringPrinted = input;
-        }
-
-        public boolean verify(String input) {
-            return input.equals(lastStringPrinted);
-        }
-    }
-
-    private class DateMock extends Date {
-
-        @Override
-        public String toString() {
-            return "Fri Nov 25 19:34:55 CET 2016";
-        }
-    }
-
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
 
     @Test
-    public void keepPrinting() throws Exception {
+    public void keepPrinting()  {
+        PrintDate printDate = new PrintDate(System.out, new Date());
+        printDate.printCurrentDate();
+    }
+
+    @Test
+    public void pPrintlnIsCalled() throws Exception {
         PrintStreamMock outMock = new PrintStreamMock(null);
         PrintDate printDate = new PrintDate(outMock, new Date());
-
         printDate.printCurrentDate();
+        assertThat(outMock.verifyPrintlnWasCalled(), is(true));
     }
-    // Prints Fri Nov 25 19:34:55 CET 2016
 
     @Test
-    public void testPrintlnIsCalled() throws Exception {
-
+    public void printlnPrintsCorrectValue() throws Exception {
         PrintStreamMock outMock = new PrintStreamMock(null);
         PrintDate printDate = new PrintDate(outMock, new DateMock());
-
         printDate.printCurrentDate();
-        assertThat(outMock.verify("Fri Nov 25 19:34:55 CET 2016"), is(true));
+        assertThat(outMock.verifyPrintlnLastCall("Fri Nov 25 19:34:55 CET 2016"), is(true));
     }
 }
