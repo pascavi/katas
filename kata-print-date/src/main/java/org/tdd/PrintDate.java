73bf31503f43a068d8e552d62d08a295e9d2e003
Pascual Cano <pascual@strata3.com>
2016-11-26 02:46:07 +0100

    moving the delegation to the production code class

--- kata-print-date/src/main/java/org/tdd/PrintDate.java
+++ kata-print-date/src/main/java/org/tdd/PrintDate.java
@@ -8,17 +8,31 @@ interface DateInterface{
     String toString();
 }
 
-
 class PrintDate {
+    //out & date private properties are the usual console out and a current date objects
+    private OutInterface out = new OutInterface() {
+        @Override
+        public void println(String input) {
+            System.out.println(input);
+        }
+    };
+    private DateInterface date = new DateInterface() {
+        @Override
+        public String toString() {
+            return new java.util.Date().toString();
+        }
+    };
 
-    private OutInterface out;
-    private DateInterface date;
-
-    PrintDate(OutInterface out, DateInterface date) {
+    //setter allowing to replace collaborators
+    void setOut(OutInterface out) {
         this.out = out;
+    }
+
+    void setDate(DateInterface date) {
         this.date = date;
     }
 
+    //code under test
     void printCurrentDate() {
         this.out.println(date.toString());
     }
--- kata-print-date/src/test/java/org/tdd/PrintDateTest.java
+++ kata-print-date/src/test/java/org/tdd/PrintDateTest.java
@@ -7,45 +7,33 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PrintDateTest {
-    private OutInterface out;
-    private DateInterface date;
+    private PrintDate printDate;
     private PrintStreamMock outMock;
     private DateMock dateMock;
 
     @Before
     public void setUp(){
-        out = new OutInterface() {
-            @Override
-            public void println(String input) {
-                System.out.println(input);
-            }
-        };
-        date = new DateInterface() {
-            @Override
-            public String toString(){
-             return new java.util.Date().toString();
-            }
-        };
+        printDate = new PrintDate();
         outMock = new PrintStreamMock();
         dateMock = new DateMock();
     }
 
     @Test
     public void keepPrinting()  {
-        PrintDate printDate = new PrintDate(out, date);
         printDate.printCurrentDate();
     }
 
     @Test
     public void printlnIsCalled() throws Exception {
-        PrintDate printDate = new PrintDate(outMock, date);
+        printDate.setOut(outMock);
         printDate.printCurrentDate();
         assertThat(outMock.verifyPrintlnWasCalled(), is(true));
     }
 
     @Test
     public void printlnPrintsCorrectValue() throws Exception {
-        PrintDate printDate = new PrintDate(outMock, dateMock);
+        printDate.setOut(outMock);
+        printDate.setDate(dateMock);
         printDate.printCurrentDate();
         assertThat(outMock.verifyPrintlnLastCall("Fri Nov 25 19:34:55 CET 2016"), is(true));
     }
